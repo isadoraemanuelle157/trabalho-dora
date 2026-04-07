3ceed54718a1fc22f8588dd9fadf55b0ff2daf36
Isadora Emanuelle <isadoraemanuelle.157@gmail.com>
2026-04-07 20:57:41 -0300

    ppp

--- Quiz/backend/config/db.js
+++ Quiz/backend/config/db.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
+    console.log('🔌 Tentando conectar no Mongo...')
+    console.log('URI:', process.env.MONGO_URI)
+
     await mongoose.connect(process.env.MONGO_URI)
 
     console.log('🟢 MongoDB conectado com sucesso!')
   } catch (error) {
-    console.error('🔴 Erro ao conectar no MongoDB:', error.message)
-    process.exit(1)
+    console.error('🔥 ERRO COMPLETO MONGO:', error)
+    // ❌ NÃO mata o servidor
   }
 }
 
